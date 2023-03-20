57ee2cefa4a4ad3f690761c298c3c3836ee81dcf
inatatsu <tatsuhiro.9699@gmail.com>
2023-03-20 22:44:13 +0900

    :+1: symbol-sdk

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,15 +1,21 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Portal, Dialog, Paragraph, Button } from 'react-native-paper'
+import { Button } from 'react-native-paper'
 import { MainStackNavProp } from '../../Router/MainRouter'
-import * as SecureStore from 'expo-secure-store'
-
+import { Account, NetworkType } from 'symbol-sdk'
 const MainScreen = () => {
   const rootNavi = useNavigation<MainStackNavProp<'Main'>>()
+
+  const acc = Account.createFromPrivateKey(
+    '891D9D7E9672925123CFB7766CE9AC740BAFED43AE78F64CE2D296F54E62E57A',
+    NetworkType.TEST_NET
+  )
+
   return (
     <View style={styles.container}>
       <Text>MainScreen</Text>
+      <Text>Address: {acc.address.plain()}</Text>
       <Button mode="contained" onPress={() => rootNavi.navigate('Sub')}>
         いどう
       </Button>
